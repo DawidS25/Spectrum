52a69d9a3fcd959094c1d0a9c9c4eca8fd6ee07b
DawidS25 <dawid.solsnia2906@op.pl>
2025-08-24 23:43:29 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -351,7 +351,7 @@ def round_info(q, current_round, current_question_number):
     with col2:
         if "virtual_board_step" not in st.session_state or st.session_state.virtual_board_step not in ["guess", "score"]:
             if st.button("🔄 Zmień pytanie"):
-                report_question(q, "questions_changed.csv", f"Zmieniono pytanie {q['id']}")
+                report_question(q, "questions/questions_changed.csv", f"Zmieniono pytanie {q['id']}")
                 new_q = draw_question()
                 if new_q:
                     st.session_state.current_question = new_q
